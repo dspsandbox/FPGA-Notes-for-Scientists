79cac475b8ce27fc2309d4d86dd808ea0e6dbe0f
pau gomez <pau.gomez@dspsandbox.org>
2021-05-12 00:27:56 +0200

    upload_tcl uses top design hw_handoff

--- tcl/upload_overlay.tcl
+++ tcl/upload_overlay.tcl
@@ -6,12 +6,13 @@ set user xilinx
 #############################
 cd [get_property DIRECTORY [current_project]]
 set projName [get_property NAME [current_project]]
+set topDesignName [string trim [get_property top [current_fileset]] "*_wrapper"]
 set overlay_dir overlay/$projName
 file delete -force $overlay_dir
 file mkdir $overlay_dir
 puts $overlay_dir
 file copy -force [glob $projName.runs/impl_1/*.bit] $overlay_dir/$projName.bit
-file copy -force [glob $projName.srcs/sources_1/bd/*/hw_handoff/*.hwh] $overlay_dir/$projName.hwh
-file copy -force [glob $projName.srcs/sources_1/bd/*/hw_handoff/*.tcl] $overlay_dir/$projName.tcl
+file copy -force [glob $projName.srcs/sources_1/bd/$topDesignName/hw_handoff/*.hwh] $overlay_dir/$projName.hwh
+file copy -force [glob $projName.srcs/sources_1/bd/$topDesignName/hw_handoff/*.tcl] $overlay_dir/$projName.tcl
 exec scp -r $overlay_dir $user@$ip:/home/xilinx/pynq/overlays
-puts "Overlay succesfuly uploaded to: \n$user@$ip:/home/xilinx/pynq/overlays/$projName"
+puts "\nOverlay \"$projName\" successfully uploaded to: \n$user@$ip:/home/xilinx/pynq/overlays/$projName"
