e0c242407bff24aa54a44d44757cc5509e04e66b
Pau Gómez <pau.gomez@dspsandbox.org>
2021-05-25 18:38:40 +0200

    Update offset_ctrl.vhd

--- hdl/offset_ctrl.vhd
+++ hdl/offset_ctrl.vhd
@@ -21,31 +21,31 @@ entity offset_ctrl is
 end offset_ctrl;
 
 architecture Behavioral of offset_ctrl is
-    signal data_1 : signed(data_width - 1 downto 0) := (others => '0');
-    signal data_2 : signed(data_width - 1 downto 0) := (others => '0');
-    signal valid_1 : std_logic := '0';
-    signal valid_2 : std_logic := '0';
+    signal data_1_reg : signed(data_width - 1 downto 0) := (others => '0');
+    signal data_2_reg : signed(data_width - 1 downto 0) := (others => '0');
+    signal valid_1_reg : std_logic := '0';
+    signal valid_2_reg : std_logic := '0';
     
 begin
     process(clk)
     begin
         if rising_edge(clk) then
             if resetn = '0' then
-                data_1 <= (others => '0');
-                data_2 <= (others => '0');
-                valid_1 <= '0';
-                valid_2 <= '0';
+                data_1_reg <= (others => '0');
+                data_2_reg <= (others => '0');
+                valid_1_reg <= '0';
+                valid_2_reg <= '0';
             else
-                data_1 <= signed(data_1_i_tdata) + signed(offset_1);
-                data_2 <= signed(data_2_i_tdata) + signed(offset_2);
-                valid_1 <= data_1_i_tvalid;
-                valid_2 <= data_2_i_tvalid;
+                data_1_reg <= signed(data_1_i_tdata) + signed(offset_1);
+                data_2_reg <= signed(data_2_i_tdata) + signed(offset_2);
+                valid_1_reg <= data_1_i_tvalid;
+                valid_2_reg <= data_2_i_tvalid;
             end if;
         end if;
     end process;
 
-    data_1_o_tdata <= std_logic_vector(data_1);
-    data_2_o_tdata <= std_logic_vector(data_2);
-    data_1_o_tvalid <= valid_1;
-    data_2_o_tvalid <= valid_2;
+    data_1_o_tdata <= std_logic_vector(data_1_reg);
+    data_2_o_tdata <= std_logic_vector(data_2_reg);
+    data_1_o_tvalid <= valid_1_reg;
+    data_2_o_tvalid <= valid_2_reg;
 end Behavioral;
