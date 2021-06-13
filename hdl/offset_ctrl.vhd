09659cf58251cb1c44573637601cf6e4ab4fa849
pau gomez <pau.gomez@dspsandbox.org>
2021-06-13 07:33:23 +0200

    update offset ctrl module

--- hdl/offset_ctrl.vhd
+++ hdl/offset_ctrl.vhd
@@ -8,44 +8,39 @@ entity offset_ctrl is
     Generic (data_width : integer := 16);
     Port ( clk : in STD_LOGIC;
            resetn : in STD_LOGIC;
-           offset_1 : in STD_LOGIC_VECTOR (data_width - 1 downto 0);
-           offset_2 : in STD_LOGIC_VECTOR (data_width - 1 downto 0);
-           data_1_i_tdata : in STD_LOGIC_VECTOR (data_width - 1 downto 0);
-           data_1_i_tvalid : in STD_LOGIC;
-           data_2_i_tdata : in STD_LOGIC_VECTOR(data_width - 1 downto 0);
-           data_2_i_tvalid : in STD_LOGIC;
-           data_1_o_tdata : out STD_LOGIC_VECTOR (data_width - 1 downto 0);
-           data_1_o_tvalid : out STD_LOGIC;
-           data_2_o_tdata : out STD_LOGIC_VECTOR (data_width - 1 downto 0);
-           data_2_o_tvalid : out STD_LOGIC);
+           offset : in STD_LOGIC_VECTOR (data_width - 1 downto 0);
+           data_i_tdata : in STD_LOGIC_VECTOR (data_width - 1 downto 0);
+           data_i_tvalid : in STD_LOGIC;
+           data_o_tdata : out STD_LOGIC_VECTOR (data_width - 1 downto 0);
+           data_o_tvalid : out STD_LOGIC);
 end offset_ctrl;
 
 architecture Behavioral of offset_ctrl is
-    signal data_1_reg : signed(data_width - 1 downto 0) := (others => '0');
-    signal data_2_reg : signed(data_width - 1 downto 0) := (others => '0');
-    signal valid_1_reg : std_logic := '0';
-    signal valid_2_reg : std_logic := '0';
+    signal sum_reg : signed(data_width - 1 downto 0) := (others => '0');
+    signal sign_data_reg : std_logic := '0';
+    signal sign_offset_reg : std_logic := '0';
+    signal valid_reg : std_logic := '0';
     
 begin
     process(clk)
     begin
         if rising_edge(clk) then
             if resetn = '0' then
-                data_1_reg <= (others => '0');
-                data_2_reg <= (others => '0');
-                valid_1_reg <= '0';
-                valid_2_reg <= '0';
+                sum_reg <= (others => '0');
+                sign_data_reg <= '0';
+                sign_offset_reg <= '0';
+                valid_reg <= '0';
             else
-                data_1_reg <= signed(data_1_i_tdata) + signed(offset_1);
-                data_2_reg <= signed(data_2_i_tdata) + signed(offset_2);
-                valid_1_reg <= data_1_i_tvalid;
-                valid_2_reg <= data_2_i_tvalid;
+                sum_reg <= signed(data_i_tdata) + signed(offset);
+                sign_data_reg <= data_i_tdata(data_width - 1);
+                sign_offset_reg <= offset(data_width - 1);
+                valid_reg <= data_i_tvalid;
             end if;
         end if;
     end process;
-
-    data_1_o_tdata <= std_logic_vector(data_1_reg);
-    data_2_o_tdata <= std_logic_vector(data_2_reg);
-    data_1_o_tvalid <= valid_1_reg;
-    data_2_o_tvalid <= valid_2_reg;
+    
+    -- Overflow protection:
+    data_o_tdata <= (data_width - 1 => sign_data_reg, others => not(sign_data_reg) ) when (sign_data_reg = sign_offset_reg) and (sign_data_reg /= sum_reg(data_width - 1)) else
+                      std_logic_vector(sum_reg);
+                      
 end Behavioral;
