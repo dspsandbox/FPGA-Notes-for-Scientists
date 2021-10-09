9ff4ea897aa229e8222097745feb6ecfa25b29b7
PauGK <paugomezkabelka@gmail.com>
2021-10-09 14:32:12 +0200

    correct tcl to support new Vivado file structure

--- tcl/upload_overlay.tcl
+++ tcl/upload_overlay.tcl
@@ -12,7 +12,7 @@ file delete -force $overlay_dir
 file mkdir $overlay_dir
 puts $overlay_dir
 file copy -force [glob $projName.runs/impl_1/*.bit] $overlay_dir/$projName.bit
-file copy -force [glob $projName.srcs/sources_1/bd/$topDesignName/hw_handoff/*.hwh] $overlay_dir/$projName.hwh
-file copy -force [glob $projName.srcs/sources_1/bd/$topDesignName/hw_handoff/*.tcl] $overlay_dir/$projName.tcl
+file copy -force [glob $projName.*/sources_1/bd/$topDesignName/hw_handoff/*.hwh] $overlay_dir/$projName.hwh
+file copy -force [glob $projName.*/sources_1/bd/$topDesignName/hw_handoff/*.tcl] $overlay_dir/$projName.tcl
 exec scp -r $overlay_dir $user@$ip:/home/xilinx/pynq/overlays
 puts "\nOverlay \"$projName\" successfully uploaded to: \n$user@$ip:/home/xilinx/pynq/overlays/$projName"
