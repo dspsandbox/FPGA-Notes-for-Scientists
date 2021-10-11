298a24af9156c956999860874e3fda041be1486d
pau gomez <pau.gomez@dspsandbox.org>
2021-10-11 11:35:09 +0200

    add channel 1/2 enabled attribute to ADC and DAC IPs

--- ip/Redpitaya-125-14-adc/xgui/adc_v1_0.tcl
+++ ip/Redpitaya-125-14-adc/xgui/adc_v1_0.tcl
@@ -1,10 +1,27 @@
 # Definitional proc to organize widgets for parameters.
 proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "Component_Name"
-  #Adding Page
-  ipgui::add_page $IPINST -name "Page 0"
+  ipgui::add_param $IPINST -name "Ch1_Enabled"
+  ipgui::add_param $IPINST -name "Ch2_Enabled"
 
+}
 
+proc update_PARAM_VALUE.Ch1_Enabled { PARAM_VALUE.Ch1_Enabled } {
+	# Procedure called to update Ch1_Enabled when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.Ch1_Enabled { PARAM_VALUE.Ch1_Enabled } {
+	# Procedure called to validate Ch1_Enabled
+	return true
+}
+
+proc update_PARAM_VALUE.Ch2_Enabled { PARAM_VALUE.Ch2_Enabled } {
+	# Procedure called to update Ch2_Enabled when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.Ch2_Enabled { PARAM_VALUE.Ch2_Enabled } {
+	# Procedure called to validate Ch2_Enabled
+	return true
 }
 
 
