4ef645178a79c5390b1e3935568e2052c64d01e9
Pau Gomez <pau.gomez@luxquanta.com>
2022-03-14 09:01:31 +0100

    add doc

---
+++ sim/sim_counter.tcl
@@ -0,0 +1,20 @@
+#Restart simulation
+restart
+
+#Define clock (100 MHz)
+add_force /counter/clk -radix hex {1 0ns} {0 5000ps} -repeat_every 10000ps 
+
+#Set counter increment = 2
+add_force /counter/incr_i -radix hex 2 
+
+#Enable core
+add_force /counter/resetn -radix hex 1 
+
+#Run 50ns
+run 50ns
+
+#Set counter increment = 4
+add_force /counter/incr_i -radix hex 4 
+
+#Run 50ns
+run 50ns
