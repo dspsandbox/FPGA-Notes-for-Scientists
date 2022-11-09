b52d3f5c4638acecbfa4c3931094b2f41d4bf68a
Pau Gomez <pau.gomez@luxquanta.com>
2022-11-09 17:19:55 +0100

    include led_link build

--- tcl/upload_overlay.tcl
+++ tcl/upload_overlay.tcl
@@ -14,5 +14,5 @@ puts $overlay_dir
 file copy -force [glob $projName.runs/impl_1/*.bit] $overlay_dir/$projName.bit
 file copy -force [glob $projName.*/sources_1/bd/$topDesignName/hw_handoff/*.hwh] $overlay_dir/$projName.hwh
 write_bd_tcl -force $overlay_dir/$projName.tcl
-exec scp -r $overlay_dir $user@$ip:/home/xilinx/pynq/overlays
+#exec scp -r $overlay_dir $user@$ip:/home/xilinx/pynq/overlays
 puts "\nOverlay \"$projName\" successfully uploaded to: \n$user@$ip:/home/xilinx/pynq/overlays/$projName"
