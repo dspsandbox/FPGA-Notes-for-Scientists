7446ec1227365e1fd2de23e393d3f8e88c43c7f9
pau gomez <paugomezkabelka@gmail.com>
2023-03-12 22:52:20 +0100

    resolve typo

--- sys/config_xilinx.sh
+++ sys/config_xilinx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 XILINX_DOWNLOAD_URL="https://onedrive.live.com/download?cid=D86684562D90EDDE&resid=D86684562D90EDDE%21141&authkey=AH4ZBCapnqsbpQc"
-INSTALLATION_DIR=~/Xilinx
+XILINX_INSTALLATION_DIR=~/Xilinx
 
 #Prerequisites (see https://support.xilinx.com/s/article/63794?language=en_US)
 sudo apt install libtinfo5 libncurses5
