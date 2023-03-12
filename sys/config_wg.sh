bc696bb387c02fb77eebfedb88c0baf78c9ed081
pau gomez <paugomezkabelka@gmail.com>
2023-03-12 17:01:17 +0100

    resolve typo

--- sys/config_wg.sh
+++ sys/config_wg.sh
@@ -3,6 +3,6 @@ sudo apt update -y
 sudo apt install -y wireguard
 sudo cp resources/wg/server/wg0.conf /etc/wireguard/wg0.conf
 sudo chown -R root:root /etc/wireguard/
-sudo chmod -R og-rwx /etc/wireguard/*
+sudo chmod -R og-rwx /etc/wireguard/
 sudo systemctl enable wg-quick@wg0.service
 sudo systemctl restart wg-quick@wg0.service
