7565f6a5fe804fab0dfa6029a48a57563dd33bc9
pau gomez <paugomezkabelka@gmail.com>
2023-03-12 21:29:19 +0100

    remove .tar.gz and  installer

--- sys/config_xilinx.sh
+++ sys/config_xilinx.sh
@@ -2,13 +2,17 @@
 XILINX_DOWNLOAD_URL="https://onedrive.live.com/download?cid=D86684562D90EDDE&resid=D86684562D90EDDE%21141&authkey=AH4ZBCapnqsbpQc"
 INSTALLATION_DIR=~/Xilinx
 
+#Prerequisites (see https://support.xilinx.com/s/article/63794?language=en_US)
+sudo apt install libtinfo5 libncurses5
+
 #Vivado installation
 mkdir -p /tmp/xilinx_download
 cd /tmp/xilinx_download
 wget --no-check-certificate -O installer.tar.gz $XILINX_DOWNLOAD_URL
 tar -xf installer.tar.gz
+rm installer.tar.gz
 cd Xilinx_Unified_*
 ./xsetup --agree 3rdPartyEULA,XilinxEULA --batch Install --edition "Vivado ML Standard" --product "Vivado" --location $XILINX_INSTALLATION_DIR
-
+rm -r Xilinx_Unified_*
 #Return to script dir
 cd "$(dirname "$BASH_SOURCE")"
