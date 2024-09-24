f0db47d925af7d9b80257a079cca82bb9517eb2b
pau gomez <paugomezkabelka@gmail.com>
2024-09-24 23:26:28 +0200

    copy board files to Xilinx installation dir

--- sys/config_xilinx.sh
+++ sys/config_xilinx.sh
@@ -15,9 +15,15 @@ cd Xilinx_Unified_*
 
 #Remove XIC
 rm -r $XILINX_INSTALLATION_DIR/xic
+
 #Remove desktop icons
 rm  ~/Desktop/*.desktop
+
 #Return to script dir
 cd "$(dirname "$BASH_SOURCE")"
+
 #Replace path for LD_LIBRARY_PATH in upload_overlay.tcl (resolve OpenSSL error)
 sed -i '/^exec/ {s#^#set ::env(LD_LIBRARY_PATH) "/usr/bin/openssl"\n#}' ../tcl/upload_overlay.tcl 
+
+#Copy board files
+cp -r ../board_files $XILINX_INSTALLATION_DIR/Vivado/*/data/boards
