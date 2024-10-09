8d46a84ca8488831150773b47b960ba943cc784a
pau gomez <paugomezkabelka@gmail.com>
2024-10-09 16:32:08 +0200

    correct dependencies

--- sys/config_xilinx_0.sh
+++ sys/config_xilinx_0.sh
@@ -3,9 +3,11 @@
 XILINX_DOWNLOAD_DIR=/tmp/xilinx_download
 XILINX_INSTALLATION_DIR=~/Xilinx
 
+#Mute needrestart
+sudo apt -y remove needrestart
+
 #Prerequisites (from https://www.hackster.io/whitney-knitter/vivado-vitis-petalinux-2024-1-install-on-ubuntu-22-04-e76e91)
 echo "dash dash/sh boolean true" | sudo debconf-set-selections
-sudo apt -y remove needrestart
 sudo dpkg --add-architecture i386
 sudo apt-get -y install gparted xinetd gawk gcc net-tools ncurses-dev openssl libssl-dev flex bison xterm autoconf libtool texinfo zlib1g-dev 
 sudo apt-get -y install iproute2 make libncurses5-dev tftpd libselinux1 wget diffstat chrpath socat tar unzip gzip python tofrodos lsb libftdi1 libftdi1-2
