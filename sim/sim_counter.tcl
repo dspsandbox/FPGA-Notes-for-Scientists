e61e1a53bfbb4dd479bd41c7e706b7a4fa8957d5
pau gomez <paugomezkabelka@gmail.com>
2025-11-25 15:50:26 +0100

    fix sim TCL

--- sim/sim_counter.tcl
+++ sim/sim_counter.tcl
@@ -1,4 +1,12 @@
+#Restart simulation
+restart
 
+#Define clock (100 MHz)
+add_force /counter/clk -radix hex {1 0ns} {0 5000ps} -repeat_every 10000ps 
+
+#Reset core
+add_force /counter/resetn -radix hex 0
+add_force /counter/incr_i -radix hex 0 
 
 #Run 50ns
 run 50ns
@@ -13,7 +21,7 @@ add_force /counter/resetn -radix hex 1
 run 50ns
 
 #Set counter increment = 4
-add_force /counter/incr_i -radix hex 0 
+add_force /counter/incr_i -radix hex 4 
 
 #Run 50ns
 run 50ns
